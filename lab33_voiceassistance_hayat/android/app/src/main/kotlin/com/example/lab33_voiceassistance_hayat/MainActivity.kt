4ea5c06e949da087d0a533b5c4f9f64fb35cb4c3
HAYAT EL ALLAOUY <hayatelallaouy01@gmail.com>
2024-01-20 13:22:48 +0100

    first commit

---
+++ lab33_voiceassistance_hayat/android/app/src/main/kotlin/com/example/lab33_voiceassistance_hayat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab33_voiceassistance_hayat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab33_voiceassistance_hayat/lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
